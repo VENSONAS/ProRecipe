bc4e3f2680892e50f47b159e6455e6fcfd7f137c
VENSONAS <gausysgabrielius@gmail.com>
2025-10-25 21:59:52 +0300

    Added Recipe and Ingredient models, DbContext and Recipe Controller

---
+++ Server/Models/Recipe.cs
@@ -0,0 +1,23 @@
+﻿namespace Server.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public List<Ingredient> Ingredients { get; set; } = new();
+        public int TotalProtein { get; set; }
+        public int TotalCalories { get; set; }
+    }
+
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public int RecipeId { get; set; }
+
+        public decimal Weight { get; set; }
+        public string Name { get; set; } = "";
+        public string Comment { get; set; } = "";
+        public int FdcId { get; set; }
+    }
+}
--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b298e7bd9bcd879a44207e3d0f049f95b61bee1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
